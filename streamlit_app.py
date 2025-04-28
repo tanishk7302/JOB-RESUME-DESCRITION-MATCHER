2ef292f800c70b327e5d1423a573a1404023d5f4
tanishk7302 <gargtanishk568@gmail.com>
2025-04-28 11:51:17 +0530

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,17 @@
+
+## Deployment
+This app is deployed live on [Streamlit Cloud](https://streamlit.io/cloud).
+
+## Tech Stack
+- Streamlit
+- Sentence Transformers (NLP)
+- Scikit-learn
+- SpaCy
+- WordCloud
+- Matplotlib
+- smtplib (for email sending)
+
+## Screenshot
+![App Screenshot](screenshot.png)
+
+---
